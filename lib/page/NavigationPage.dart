a511f1c6316618c177b8374e480d035567de7894
nidalee <1936641791@qq.com>
2018-09-14 17:13:33 +0800

    刷新

--- lib/page/NavigationPage.dart
+++ lib/page/NavigationPage.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 
 import 'package:flutter/material.dart';
 
@@ -7,17 +8,76 @@ class NavigationWidget extends StatefulWidget {
 }
 
 class _NavigationWidgetState extends State<NavigationWidget> {
+  ScrollController _controller = new ScrollController();
+
+  List _dataList = [];
+
+  bool _isNeedLoadMore = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _listAddData();
+    _controller.addListener(() {
+      if (_controller.position.pixels == _controller.position.maxScrollExtent && _isNeedLoadMore) {
+        _loadmore();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
+    return new Scaffold(
         appBar: new AppBar(
           title: new Text("热门导航"),
         ),
-        body: Center(
-          child: Text("热门导航"),
-        ),
-      ),
-    );
+        body: new RefreshIndicator(
+            color: Colors.redAccent,
+            backgroundColor: Colors.green,
+            child: new Center(
+              child: ListView.builder(
+                itemBuilder: (context, position) {
+                  if (position == _dataList.length) {
+                    return new Text("加载");
+                  } else {
+                    return new Card(
+                      child: new Container(
+                        height: 80.0,
+                        child: new Text("item：$position"),
+                      ),
+                    );
+                  }
+                },
+                itemCount: _dataList.length + 1,
+                controller: _controller,
+              ),
+            ),
+            onRefresh: _refresh));
+  }
+
+  _listAddData() {
+    _dataList = List.generate(20, (index) {});
+  }
+
+  Future<Null> _loadmore() async {
+    await Future.delayed(Duration(seconds: 2), () {
+      setState(() {
+        _dataList.add(1);
+        _dataList.add(1);
+        _dataList.add(1);
+        _dataList.add(1);
+        _dataList.add(1);
+      });
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 3), () {
+      _dataList.clear();
+      setState(() {
+        _listAddData();
+      });
+    });
   }
 }
